e94fb7df3af62033220e3698ed4c64b4b080768b
swh5620 <swh5620@gmail.com>
2026-07-10 13:49:24 +0900

    '2026-07-10'

--- app.py
+++ app.py
@@ -1,31 +1,61 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, send_file, redirect
 from scrapper import search_incruit, search_work24
+from file import save_to_csv
 
 
 app = Flask(__name__)
 
+db = {}
+page = 5
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/search")
 def search():
+    keyword = request.args.get("keyword")
 
-	keyword = request.args.get("keyword")
+    if keyword == "":
+        return redirect("/")
 
-	incruit_jobs = search_incruit(keyword)
-	work24_jobs = search_work24(keyword)
+    incruit_jobs = search_incruit(keyword, page)
+    work24_jobs = search_work24(keyword)
 
-	jobs = incruit_jobs + work24_jobs
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = incruit_jobs + work24_jobs
+        db[keyword] = jobs
 
-	return render_template(
-		"search.html",
-		jobs=enumerate(jobs)
-	)
+    return render_template(
+        "search.html",
+        jobs=enumerate(jobs),
+        keyword=keyword,
+        count=len(jobs)
+    )
+
+
+@app.route("/file")
+def file():  # 키워드가 있으면 if문으로 처리 빠르게 하기
+    keyword = request.args.get("keyword")
+
+    if keyword == "":
+        return redirect("/")
+
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = search_incruit(keyword, page)
+
+    save_to_csv(jobs)
+
+    return send_file(
+        "./downloads.csv",
+        as_attachment=True
+    )
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -2,85 +2,103 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def search_incruit(keyword):
-
-	url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
-
-	r = requests.get(url)
-
-	soup = BeautifulSoup(r.text, "html.parser")
-
-	lis = soup.find_all("li", class_="c_col")
-
+def search_incruit(keyword, page=1):
+    # 1 -> 0
+	# 2 -> 30
+	# 3 -> 60
 	jobs = []
+	for i in range(page): #page에 3이 들어가면 i는 0, 1, 2가 들어감 
+		page = 30 * i
+		url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+		r = requests.get(url)
+		soup = BeautifulSoup(r.text, "html.parser")
+		lis = soup.find_all("li", class_="c_col")
 
-	for li in lis:
+		for li in lis:
+			company = li.find(
+				"a",
+				class_="cpname"
+			).text.strip()
 
-		company = li.find(
-			"a",
-			class_="cpname"
-		).text.strip()
+			title = (
+				li.find("div", class_="cell_mid")
+				.find("div", class_="cl_top")
+				.find("a")
+				.text.strip()
+			)
 
-		title = (
-			li.find("div", class_="cell_mid")
-			.find("div", class_="cl_top")
-			.find("a")
-			.text.strip()
-		)
+			location = (
+				li.find("div", class_="cl_md")
+				.find_all("span")[0]
+				.text.strip()
+			)
 
-		location = (
-			li.find("div", class_="cl_md")
-			.find_all("span")[0]
-			.text.strip()
-		)
-
-		link = (
-			li.find("div", class_="cell_mid")
-			.find("div", class_="cl_top")
-			.find("a")
-			.get("href")
-		)
-
-		job_data = {
-			"company": company,
-			"title": title,
-			"location": location,
-			"link": link
-		}
-
-		jobs.append(job_data)
-
-	return jobs
-
-
-
-def search_work24(keyword):
-
-	url = f"https://www.work24.go.kr/wk/a/b/1200/retriveDtlEmpSrchList.do?searchMode=Y&srcKeyword={keyword}&pageIndex=1&resultCnt=20
-
-	r = requests.get(url)
-
-	soup = BeautifulSoup(r.text, "html.parser")
-
-	trs = soup.find_all("tr")
-
-	jobs = []
-
-	for tr in trs:
-
-		company = tr.find("a", class_="cp_name")
-		title = tr.find("a", class_="t3_sb")
-		location = tr.find("li", class_="site")
-
-		if company and title and location:
+			link = (
+				li.find("div", class_="cell_mid")
+				.find("div", class_="cl_top")
+				.find("a")
+				.get("href")
+			)
 
 			job_data = {
-				"company": company.text.strip(),
-				"title": title.text.strip(),
-				"location": location.find("p").text.strip(),
-				"link": "https://www.work24.go.kr" + title.get("href")
+				"company": company,
+				"title": title,
+				"location": location,
+				"link": link
 			}
 
 			jobs.append(job_data)
 
 	return jobs
+
+if __name__ == "__main__":
+	result = search_incruit("간호사", 2)
+	print(result)
+	print(len(result))
+
+def search_work24(keyword):
+
+    url = f"https://www.work24.go.kr/wk/a/b/1200/retriveDtlEmpSrchList.do?searchMode=Y&srcKeyword={keyword}&pageIndex=1&resultCnt=20"
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    trs = soup.select("tr:has(a.cp_name)") #선택 조건
+
+    jobs = []
+
+    for tr in trs:
+
+        company = (
+            tr.find("a", class_="cp_name")
+            .text.strip()
+        ) #여기서 a는 태그 찾기  
+        
+        title = (
+            tr.find("a", class_="t3_sb")
+            .text.strip()
+        )
+
+        location = (
+            tr.find("li", class_="site")
+            .find("p")
+            .text.strip()
+        )
+
+        link = (
+            "https://www.work24.go.kr"
+            + tr.find("a", class_="t3_sb")
+            .get("href") #주소 가져오는 함수
+        )
+
+        job_data = {
+            "company": company,
+            "title": title,
+            "location": location,
+            "link": link
+        }
+
+        jobs.append(job_data)
+
+    return jobs
